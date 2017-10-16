f205d9d86b04f57357dbb6be0b41ec285663851d
Alina Nezhivinskaya <alina.nezhivinskaya@mail.ru>
2017-10-16 15:32:44 +0300

    task3: 1,2

---
+++ task3/2/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		RandomNumber randomNumber=new RandomNumber(3);
+		System.out.println(randomNumber.getNumber());
+		System.out.println("sum="+randomNumber.getDigitsSum());
+
+	}
+
+}
